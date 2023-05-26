35de39699d53e570fc8325f35e4646152d921b93
BharathSanjeeviT <bharathsanjeevi.t@gmail.com>
2023-05-26 10:52:10 +0530

    login and user details

---
+++ app/api/details/route.ts
@@ -0,0 +1,16 @@
+import { prisma } from "@/prisma/prisma"
+import {NextRequest, NextResponse} from "next/server"
+
+export async function POST( req:NextRequest ) {
+    const { reg,name, location , branch} = await req.json()
+    console.log(name,branch,location)
+    const current = await prisma.user.update({
+        where: {
+            reg,
+        },
+        data : {
+            name,branch,location
+        },
+    })
+    return NextResponse.json({current})
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,29 +1,41 @@
 'use client'
 import Notification from "@/components/Notification"
 import axios from "axios"
+import { useRouter } from "next/navigation"
 import {useEffect, useState} from "react"
+import './globals.css'
 
 const Home = () => {
+
 
     const [reg, setReg] = useState('')
     const [pass, setPass] = useState('')
     const[data,setData] = useState('')
     const[status,setStatus] = useState('')
     const[notify,setNotify] = useState(false)
+    const [spinner , setSpinner] = useState(false)
 
+    const router = useRouter()
     useEffect(()=>{
-        setTimeout(()=>{
+        const timer = setTimeout(()=>{
             setNotify(false)
+            if(status === "sucess"){
+                localStorage.setItem("user",reg)
+                router.push('/home')
+            }
         },2000)
+        return () => clearTimeout(timer)
     },[notify])
 
     const handleLogin = async (e:any) => {
         e.preventDefault()
+        setSpinner(true)
 
         if((reg==="")||(pass==="")){
                 setData("Fill the credentials")
                 setStatus("error")
                 setNotify(true)
+                setSpinner(false)
         }else{
             const { data } = await axios.post('/api', { reg, pass })
             console.log(data)
@@ -31,6 +43,7 @@ const Home = () => {
                 setData("Password incorrect")
                 setStatus("error")
                 setNotify(true)
+                setSpinner(false)
             }
             else if ( data.status == '1' ) {
                 setData("Logged In sucessfully")
@@ -41,6 +54,7 @@ const Home = () => {
                 setData("User Unavailable")
                 setStatus("error")
                 setNotify(true)
+                setSpinner(false)
             }
         }
     }
@@ -48,16 +62,25 @@ const Home = () => {
     return (
         <>
         {notify && <Notification status={status} data={data} />}
-        <div className="flex items-center justify-center w-full h-full">
-            <form onSubmit={handleLogin} className="border-2 border-black rounded-md p-4 h-[65%] w-[25%] flex items-center justify-center">
+        <div className="flex items-center justify-center w-[100vw] h-[100vh]">
+            <form onSubmit={handleLogin} className="flex items-center justify-center p-4 border shadow-md rounded-md">
                 <div>
-                    <label className="font-bold text-[1.5rem]">Reg. No: &nbsp;&nbsp;&nbsp;&nbsp; <input onChange={e => setReg(e.target.value)} className="p-2 m-2 border border-black rounded" type="text"/></ label>
+                    <label className="text-[#454443]">Reg. No: <br/><input onChange={e => setReg(e.target.value)} className="p-2 text-black border border-black rounded" type="text"/></ label>
                     <br/>
                     <br/>
-                    <label className="font-bold text-[1.5rem]">Password: <input onChange={e => setPass(e.target.value)} className="p-2 m-2 border border-black rounded" type="password"/></ label>
+                    <label className="text-[#454443]">Password: <br/><input onChange={e => setPass(e.target.value)} className="p-2 text-black border border-black rounded" type="password"/></ label>
                     <br/>
                     <br/>
-                    <button type='submit' className="px-3 py-2 font-bold text-black bg-transparent border-2 border-black transition-all rounded-md hover:bg-black hover:text-white">LOGIN</button>
+                    <button type='submit' className="w-32 px-3 py-2 font-bold text-black bg-transparent border-2 border-black transition-all rounded-md hover:bg-black hover:text-white">
+                    {spinner ?
+                    <div className="flex items-center justify-center h-6">
+                        <div className="animate-spin inline-block w-6 h-6 border-[3px] border-current border-t-transparent text-black rounded-full">
+                        <span className="sr-only">Loading...</span>
+                        </div>
+                     </div> :
+                     <p>Login</p>
+                    }
+                    </button>
                 </div>
             </form>
         </div>
